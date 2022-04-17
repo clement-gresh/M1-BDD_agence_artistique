9eab1a4c14903142cb3d0d1101e31ac3815c48e4
clement-gresh <clem_paname@hotmail.com>
2022-04-17 12:28:57 +0200

    feature : added trigger on Involvments to check the artist has the skill in KnownSkills

--- Creation/cmd-clem.sql
+++ Creation/cmd-clem.sql
@@ -101,7 +101,6 @@ CREATE TABLE Involvments(
 	CONSTRAINT Involvments_creation_id_fkey FOREIGN KEY (creation_id) REFERENCES project_db_2021.Creations (creation_id),
 	CONSTRAINT Involvments_skill_id_fkey FOREIGN KEY (skill_id) REFERENCES project_db_2021.Skills (skill_id)
 );
--- trigger : verifier que skill_name apparait bien dans KnownSkills pour ce contact
 
 
 
@@ -117,16 +116,16 @@ CREATE TABLE KnownSkills(
 
 -- Involvments : checks that the skill referenced is of type 'job'
 CREATE OR REPLACE FUNCTION is_skill_job() RETURNS TRIGGER AS $$
-   declare 
+   DECLARE 
       nb INT;
 	BEGIN
-		SELECT Count(*) INTO nb
+		SELECT count(*) INTO nb
 		FROM Involvments INNER JOIN Skills
 			ON Skills.skill_id = Involvments.skill_id
 		WHERE Involvments.skill_id = NEW.skill_id AND Skills.skill_type = 'job'::skill_type_enum;
 		
 		IF (nb = 0) THEN
-			RAISE NOTICE 'Rejected line ("%", "%", "%") because the skill is not of type job',
+			RAISE NOTICE 'Rejected line ("%", "%", "%") because the skill is not of type job.',
 				NEW.contact_id, NEW.creation_id, NEW.skill_id;
 			RETURN NULL;
 		ELSE
@@ -141,8 +140,30 @@ FOR EACH ROW
 EXECUTE PROCEDURE is_skill_job();
 
 -- Involvments : checks in KnownSkills that the artist does have the skill referenced
--- CREATE OR REPLACE has_skill() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION has_skill() RETURNS TRIGGER AS $$
+	DECLARE
+		nb INT;
+	BEGIN
+		SELECT count(*) INTO nb
+		FROM Involvments INNER JOIN KnownSkills -- est-ce toutes les skills de chaque artiste apparraissent ? Ou faut-il LEFT/RIGHT JOIN ?
+			ON Involvments.contact_id = KnownSkills.contact_id
+		WHERE NEW.contact_id = KnownSkills.contact_id AND NEW.skill_id = KnownSkills.skill_id;
+		
+		IF (nb = 0) THEN
+			RAISE NOTICE 'Rejected line ("%", "%", "%") because the artist does not appear to have that skill (see KnownSkills).',
+				NEW.contact_id, NEW.creation_id, NEW.skill_id;
+			RETURN NULL;
+		ELSE
+			RETURN NEW;
+		END IF;
+		RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER Involvments_skill_id_trigger
+BEFORE INSERT OR UPDATE ON Involvments
+FOR EACH ROW
+EXECUTE PROCEDURE has_skill();
 
 
 \dt

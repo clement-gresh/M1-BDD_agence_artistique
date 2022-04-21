83906064110584a04a445da11bf6e81c912a426e
clement-gresh <clem_paname@hotmail.com>
2022-04-21 11:33:09 +0200

    amelioration : renaming some files before merging

--- Creation/create_all.sql
+++ Creation/create_all.sql
@@ -1,4 +1,10 @@
--- command to execute in psql: \i 'C:/Users/Clem/01-coding-projects/08-sql-projects/projet-bdd-2021/Creation/cmd-clem.sql'
+/*
+command to execute in psql:
+\i 'C:/Users/Clem/01-coding-projects/08-sql-projects/projet-bdd-2021/Creation/create_all_clem.sql'
+\i 'C:/Users/Clem/01-coding-projects/08-sql-projects/projet-bdd-2021/Creation/create_triggers_clem.sql'
+\i 'C:/Users/Clem/01-coding-projects/08-sql-projects/projet-bdd-2021/Creation/insert_data_clem.sql'
+
+*/
 -- <>
 
 SET search_path TO postgres;
@@ -20,10 +26,6 @@ CREATE TYPE skill_name_enum AS ENUM (
 	'french', 'english', 'arabic', 'spanish', 'german', 'italian', 'mandarin', 'hindi', 'japanese',
 	'jazz', 'classical', 'RandB', 'rock', 'soul', 'rap', 'slam', 'metal'
 );
--- CREATE TYPE job_enum AS ENUM ('writer', 'musician', 'singer', 'actor', 'director', 'producer');
--- CREATE TYPE instrument_enum AS ENUM ('violin', 'guitar', 'saxophone', 'piano', 'trumpet', 'flute');
--- CREATE TYPE language_enum AS ENUM ('french', 'english', 'arabic', 'spanish', 'german', 'italian', 'mandarin', 'hindi', 'japanese');
--- CREATE TYPE style_enum AS ENUM ('jazz', 'classical', 'RandB', 'rock', 'soul', 'rap', 'slam', 'metal');
 
 
 -- TABLES
@@ -36,19 +38,20 @@ CREATE TABLE Agents (
 	birth_date DATE NOT NULL,
 	tel VARCHAR(20) NOT NULL,
 	address TEXT NOT NULL,
+	city VARCHAR(50) NOT NULL,
 	postal_code VARCHAR(8) NOT NULL,
 	CONSTRAINT Agents_pkey PRIMARY KEY (agent_id),
-	CONSTRAINT email_check CHECK (email ~* '^[a-z0-9._-]+@[a-z0-9._-]{2,100}\.[a-z]{2,4}$'),
+	CONSTRAINT email_check CHECK (email ~* '^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,100}\.[a-z]{2,4}$'),
 	CONSTRAINT birth_date_check CHECK (birth_date > '1900-01-01' AND birth_date < NOW()),
-	CONSTRAINT tel_check CHECK (tel ~* '^(+)?[0-9\)\(]{10,20}$'),
+	CONSTRAINT tel_check CHECK (tel ~* '^(\+)?[0-9\)\(]{10,20}$'),
 	CONSTRAINT postal_code_check CHECK (postal_code ~* '^[0-9]{2,8}$')
 );
 
 CREATE TABLE AgencyContracts(
 	contact_id INT NOT NULL,
-	start_date DATE NOT NULL,
-	end_date DATE,
-	fee REAL NOT NULL,
+	start_date DATE NOT NULL,	-- contract_start
+	end_date DATE,				--contract_end
+	fee NUMERIC(6,4) NOT NULL,
 	CONSTRAINT AgencyContracts_pkey PRIMARY KEY (contact_id, start_date),
 	-- CONSTRAINT agency_contracts_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES project_db_2021.Contacts (contact_id),
 	CONSTRAINT start_date_check CHECK (start_date > '2000-01-01' AND start_date < '2100-01-01'),
@@ -59,8 +62,8 @@ CREATE TABLE AgencyContracts(
 CREATE TABLE AgentRecords(
 	agent_id INT NOT NULL,
 	contact_id INT NOT NULL,
-	start_date DATE NOT NULL,
-	end_date DATE,
+	start_date DATE NOT NULL, -- represent_start
+	end_date DATE,				-- represent_end
 	CONSTRAINT AgentRecord_pkey PRIMARY KEY (agent_id, contact_id),
 	CONSTRAINT agent_record_agent_id_fkey FOREIGN KEY (agent_id) REFERENCES project_db_2021.Agents (agent_id),
 	-- CONSTRAINT agent_record_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES project_db_2021.Contacts (contact_id),
@@ -103,7 +106,6 @@ CREATE TABLE Involvments(
 );
 
 
-
 CREATE TABLE KnownSkills(
 	contact_id INT NOT NULL,
 	skill_id INT NOT NULL,
@@ -112,58 +114,6 @@ CREATE TABLE KnownSkills(
 	CONSTRAINT KnownSkills_skill_id_fkey FOREIGN KEY (skill_id) REFERENCES project_db_2021.Skills (skill_id)
 );
 -- trigger : seul un musicien peut avoir un skill_type = instrument ou style
-
-
--- Involvments : checks that the skill referenced is of type 'job'
-CREATE OR REPLACE FUNCTION is_skill_job() RETURNS TRIGGER AS $$
-   DECLARE 
-      nb INT;
-	BEGIN
-		SELECT count(*) INTO nb
-		FROM Involvments INNER JOIN Skills
-			ON Skills.skill_id = Involvments.skill_id
-		WHERE Involvments.skill_id = NEW.skill_id AND Skills.skill_type = 'job'::skill_type_enum;
-		
-		IF (nb = 0) THEN
-			RAISE NOTICE 'Rejected line ("%", "%", "%") because the skill is not of type job.',
-				NEW.contact_id, NEW.creation_id, NEW.skill_id;
-			RETURN NULL;
-		ELSE
-			RETURN NEW;
-		END IF;
-	END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER Involvments_skill_id_trigger
-BEFORE INSERT OR UPDATE ON Involvments
-FOR EACH ROW
-EXECUTE PROCEDURE is_skill_job();
-
--- Involvments : checks in KnownSkills that the artist does have the skill referenced
-CREATE OR REPLACE FUNCTION has_skill() RETURNS TRIGGER AS $$
-	DECLARE
-		nb INT;
-	BEGIN
-		SELECT count(*) INTO nb
-		FROM Involvments INNER JOIN KnownSkills -- est-ce toutes les skills de chaque artiste apparraissent ? Ou faut-il LEFT/RIGHT JOIN ?
-			ON Involvments.contact_id = KnownSkills.contact_id
-		WHERE NEW.contact_id = KnownSkills.contact_id AND NEW.skill_id = KnownSkills.skill_id;
-		
-		IF (nb = 0) THEN
-			RAISE NOTICE 'Rejected line ("%", "%", "%") because the artist does not appear to have that skill (see KnownSkills).',
-				NEW.contact_id, NEW.creation_id, NEW.skill_id;
-			RETURN NULL;
-		ELSE
-			RETURN NEW;
-		END IF;
-		RETURN NEW;
-	END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER Involvments_skill_id_trigger
-BEFORE INSERT OR UPDATE ON Involvments
-FOR EACH ROW
-EXECUTE PROCEDURE has_skill();
 
 
 \dt
@@ -182,10 +132,8 @@ EXECUTE PROCEDURE has_skill();
 
 
 -- PROFS
--- demander rdv
--- Qu'est ce qu'un schema ?
--- ENGINE = INNODB ???
 -- quelle difference entre faire un check(function) et un TRIGGER(function) ? Quand les 2 sont possibles, lequel est preferable ?
+-- dans un check, une fonction doit toujours retourner le meme resultat pour un meme contenu de ligne (ce qui n'est pas le cas pour un trigger).
+-- Si on met une fonction dans un check, il peut donc y avoir de "l'etat cache" car on peut utiliser les valeurs d'autres tables. Il faut donc mettre
+-- les fonctions dans les triggers et non dans les check.
 
-
--- KnownSkills
--- Creation/insert_data_clem.sql
+++ Creation/insert_data_clem.sql
@@ -3,6 +3,18 @@
 -- UPDATE Contacts SET tel = '+33' || to_char( 600000000 + random() * 200000000 + 1, 'FM999999999') ;
 
 /*
+insert into towns (
+    code, article, name, department
+)
+select
+    left(md5(i::text), 10),
+    md5(random()::text),
+    md5(random()::text),
+    left(md5(random()::text), 4)
+from generate_series(1, 1000000) s(i)
+*/
+
+/*
 function (line) {
 	let numero = '+33';
 	for(var i = 1; i < 9; i++){

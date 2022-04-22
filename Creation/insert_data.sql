993541b4cc9975a05da18eacf6de4db1358ffbff
Li-FangSu <su.lifang@outlook.com>
2022-04-22 16:12:04 +0200

    Creation : insert data of the table contacts

---
+++ Creation/insert_data.sql
@@ -0,0 +1,44 @@
+
+--Contacts
+COPY contacts(first_name, last_name, email, gender)
+FROM '/Users/sulifang/Projets/projet-bdd-2021/Creation/contacts.csv'
+DELIMITER ','
+CSV HEADER;
+
+UPDATE contacts SET society = 'Studio ' || UPPER(last_name) WHERE contact_id IN (SELECT contact_id FROM contacts ORDER BY RANDOM() LIMIT 1000);
+
+update contacts set birth_date =  TO_date(to_char(  1+random() *27, '00') || '/' || to_char( 1+random() *11, '00') || '/' ||  to_char( 1921 +random() *80, '0000') ,'DD/ MM/ YYYY' );
+
+UPDATE contacts SET tel= '+33' || to_char( 600000000 + random() * 200000000 + 1, 'FM999999999') ;
+ALTER TABLE contacts ALTER COLUMN tel SET NOT NULL;
+
+-- 1+random()*99 genere des ##
+UPDATE contacts SET address = to_char(  1+random()*98, '00') || ' rue de ' || last_name;
+
+UPDATE contacts SET postal_code = 1+random() *99999;
+
+update contacts set city =  (array['Paris', 'Strasbourg', 'Tours', 'Lille', 'Chicago', 
+                                   'London', 'Berlin', 'Tokyo', 'New York', 'Marseille', 
+                                   'Toulouse', 'Nice', 'Los Angeles', 'Versailles', 'Houston', 
+                                   'Seoul', 'Seattle', 'Manchester', 'Toronto', 'Prague'
+                                   'Taipei', 'Havana', 'New Delhi', 'Rome', 'Manila',
+                                   'Moscow', 'Sydney', 'Dubai', 'Madrid', 'Osaka'])[floor(random() * 30 + 1)];
+SELECT * FROM contacts LIMIT 5;
+
+-- Requests
+INSERT INTO requests
+=> CHECK PRODCONTACT has skill procuder , else give it to him
+INSERT ON REQUIREDSKILL
+
+
+--     request_id SERIAL CONSTRAINT Requests_request_id_pk PRIMARY KEY,
+--     contact_id INTEGER,
+--     creation_id INTEGER, 
+--     description TEXT, 
+--     budget NUMERIC (12,2) NOT NULL CHECK(budget >=0),  --trigger >=0
+--     request_status requests_status_type NOT NULL, 
+--     request_start DATE NOT NULL, 
+--     request_end DATE,
+--     CONSTRAINT Requests_contact_id_fk FOREIGN KEY (contact_id) REFERENCES project_db_2021.Contacts (contact_id),
+--     CONSTRAINT Creations_creation_id_fk FOREIGN KEY (creation_id) REFERENCES project_db_2021.Creations (creation_id),
+--     CHECK(request_end >= request_start)

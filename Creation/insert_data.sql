fa72230dfc627beca0a6b1c22531dadadfa0f689
Li-FangSu <su.lifang@outlook.com>
2022-04-24 10:05:05 +0200

    Creation : moved requests into another file

--- Creation/insert_data.sql
+++ Creation/insert_data.sql
@@ -12,7 +12,7 @@ update contacts set birth_date =  TO_date(to_char(  1+random() *27, '00') || '-'
 UPDATE contacts SET tel= '+33' || to_char( 600000000 + random() * 200000000 + 1, 'FM999999999') ;
 ALTER TABLE contacts ALTER COLUMN tel SET NOT NULL;
 
--- 1+random()*99 genere des ##
+-- 1+random()*98 avoid generating ##
 UPDATE contacts SET address = to_char(  1+random()*98, '00') || ' rue de ' || last_name;
 
 UPDATE contacts SET postal_code = 1+random() *99999;
@@ -24,37 +24,3 @@ update contacts set city =  (array['Paris', 'Strasbourg', 'Tours', 'Lille', 'Chi
                                    'Taipei', 'Havana', 'New Delhi', 'Rome', 'Manila',
                                    'Moscow', 'Sydney', 'Dubai', 'Madrid', 'Osaka'])[floor(random() * 30 + 1)];
 SELECT * FROM contacts LIMIT 5;
-
---Requests
---insert data of requests randomly
-CREATE OR REPLACE FUNCTION insert_reuqests() RETURNS void AS $$
-DECLARE
-    i INTEGER; --330 open
-    nb_contacts INTEGER;
-BEGIN
-    nb_contacts := (SELECT count(*) from contacts);
-    FOR i IN 1..nb_contacts/3
-    LOOP
-        --the value of contact_id is between 1 and nb_contacts randomly
-        --for having more open in requests
-        --floor(...) + 1 to avoid 0
-        INSERT INTO Requests(contact_id, request_status, budget, request_end) 
-        VALUES(floor(RANDOM()*nb_contacts)+1, (ARRAY['open', 'closed', 'cancelled', 'open', 'open'])[floor(random() * 5 + 1)]::requests_status_type, RANDOM()*10000, NOW()+INTERVAL '1 day' * random()*365);
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT insert_reuqests();
-SELECT * FROM Requests LIMIT 5;
-
-    -- request_id SERIAL CONSTRAINT Requests_request_id_pk PRIMARY KEY,
-    -- contact_id INTEGER NOT NULL, --trigger vérifier que contact a un skill_type : job : producteur
-    -- creation_id INTEGER, --NOT NULL, 
-    -- request_description TEXT, 
-    -- budget NUMERIC (12,2) NOT NULL CHECK(budget >=0),  --trigger >=0
-    -- request_status requests_status_type NOT NULL, 
-    -- request_start DATE NOT NULL DEFAULT NOW(), 
-    -- request_end DATE,
-    -- CONSTRAINT Requests_contact_id_fk FOREIGN KEY (contact_id) REFERENCES project_db_2021.Contacts (contact_id),
-    -- --CONSTRAINT Creations_creation_id_fk FOREIGN KEY (creation_id) REFERENCES project_db_2021.Creations (creation_id),
-    -- CHECK(request_end >= request_start)

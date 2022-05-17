1bba31a44d39f81debc0eab5b25ffe4f80e2c92f
Li-FangSu <su.lifang@outlook.com>
2022-05-17 03:50:09 +0200

    Creation : modified function of requests

--- Creation/creation_function.sql
+++ Creation/creation_function.sql
@@ -7,23 +7,25 @@ DECLARE
     nb_contacts INTEGER;
     cid INTEGER;
     ran INTEGER;
+    dat date;
 BEGIN
     nb_contacts := (SELECT count(*) from contacts);
     FOR i IN 1..nb_contacts/3
     LOOP
-        SELECT creation_id INTO cid FROM creations ORDER BY RANDOM() LIMIT 1;
+        SELECT creation_id,release_date INTO cid,dat FROM creations ORDER BY RANDOM() LIMIT 1;
         --the value of contact_id is between 1 and nb_contacts randomly
         --for having more open in requests
         --floor(...) + 1 to avoid 0
         ran := floor(RANDOM()*10+1);
         FOR j IN 1..ran
         LOOP
-            INSERT INTO Requests(contact_id, creation_id, request_status, budget, request_end) 
+            INSERT INTO Requests(contact_id, creation_id, request_status, budget,request_start, request_end) 
             VALUES( floor(RANDOM()*nb_contacts)+1, 
                     cid, 
                     (ARRAY['open', 'closed', 'cancelled', 'open', 'open'])[floor(random()*5+1)]::requests_status_type, 
                     RANDOM()*10000, 
-                    NOW() + INTERVAL '1 day' *random()*365
+                    dat - INTERVAL '1 day' *(random()*100 +100),
+                    dat - INTERVAL '1 day' *random()*100
                    );
         END LOOP;
     END LOOP;
